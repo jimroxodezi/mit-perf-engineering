953a33e5399c77c331da654043c6e4e935c42417
jimroxodezi <jimroxodezi@gmail.com>
2023-01-20 22:21:07 +0100

    updated python matrix

--- 1-introduction/python/matrix.py
+++ 1-introduction/python/matrix.py
@@ -1,11 +1,12 @@
 import time, random
 
 
-n = 1024
+ROW_SIZE = 1024
+COLUMN_SIZE = 1024
 
-A = [[random.random() for row in range(n)] for column in range(n)]
-B = [[random.random() for row in range(n)] for column in range(n)]
+A = [[random.random() for row in range(ROW_SIZE)] for column in range(COLUMN_SIZE)]
+B = [[random.random() for row in range(ROW_SIZE)] for column in range(COLUMN_SIZE)]
 
-C = [[0 for row in range(n)] for colunm in range(n)]
+C = [[0 for row in range(ROW_SIZE)] for colunm in range(COLUMN_SIZE)]
 
 
