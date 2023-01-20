b69d79deeb4ccbdf6ea6228c6f0baa99ad355c55
jimroxodezi <jimroxodezi@gmail.com>
2023-01-20 22:41:33 +0100

    updated python matrix

--- 1-introduction/python/matrix.py
+++ 1-introduction/python/matrix.py
@@ -1,12 +1,22 @@
 import time, random
 
 
-ROW_SIZE = 1024
-COLUMN_SIZE = 1024
+MATRIX_SIZE = 1024
+# COLUMN_SIZE = 1024
 
-A = [[random.random() for row in range(ROW_SIZE)] for column in range(COLUMN_SIZE)]
-B = [[random.random() for row in range(ROW_SIZE)] for column in range(COLUMN_SIZE)]
+A = [[random.random() for row in range(MATRIX_SIZE)] for column in range(MATRIX_SIZE)]
+B = [[random.random() for row in range(MATRIX_SIZE)] for column in range(MATRIX_SIZE)]
 
-C = [[0 for row in range(ROW_SIZE)] for colunm in range(COLUMN_SIZE)]
+C = [[0 for row in range(MATRIX_SIZE)] for colunm in range(MATRIX_SIZE)]
+
+start = time.time()
+for i in range(MATRIX_SIZE):
+    for j in range(MATRIX_SIZE):
+        for k in range(MATRIX_SIZE):
+            C[i][j] = A[i][k] * B[j][k]
+end = time.time()
 
 
+print(end-start)
+# print(B)
+# print(C)
