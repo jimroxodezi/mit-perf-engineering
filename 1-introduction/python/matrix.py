c413d98421caa07348e925bf51529fc179d1dbc4
jimroxodezi <jimroxodezi@gmail.com>
2023-01-20 22:13:59 +0100

    first commit

---
+++ 1-introduction/python/matrix.py
@@ -0,0 +1,11 @@
+import time, random
+
+
+n = 1024
+
+A = [[random.random() for row in range(n)] for column in range(n)]
+B = [[random.random() for row in range(n)] for column in range(n)]
+
+C = [[0 for row in range(n)] for colunm in range(n)]
+
+
