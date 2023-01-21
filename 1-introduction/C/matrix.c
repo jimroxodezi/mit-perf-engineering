e6cd4f44b6bb3eb0aaeadba6de17e0ea23d14e28
jimroxodezi <jimroxodezi@gmail.com>
2023-01-21 06:57:38 +0100

    updated C matrix

---
+++ 1-introduction/C/matrix.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/time.h>
+
+#define MATRIX_SIZE 1024
+
+int main(void) {
+    for (int i = 0; i < MATRIX_SIZE; i++) {
+        for (int j = 0; j < MATRIX_SIZE; j++) {
+            for (int k = 0; k < MATRIX_SIZE; k++) {
+                
+            }
+        }
+    }
+}
